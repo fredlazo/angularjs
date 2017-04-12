21f126cc38f721137716cd98302ea27a0b4483aa
Frederick Lazo <fredlazo@yahoo.com>
2017-04-11 19:23:18 -0700

    second commit

---
+++ app/app.js
@@ -0,0 +1,63 @@
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+/*
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/home', {
+            templateurl: 'views/home.html'
+        }),
+        .when('/directory', {
+            templateurl: 'views/directory.html',
+            controller: 'NinjaController'
+        }).otherwise({
+            redirectTo: '/home'
+        })
+}]);
+*/
+
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+        $scope.removeNinja = function(ninja){
+            var removedNinja = $scope.ninjas.indexOf(ninja);
+            $scope.ninjas.splice(removedNinja,1);
+        };
+        $scope.addCar = function(){
+            $scope.ninjas.push({
+                name: $scope.newcar.name,
+                color: $scope.newcar.color,
+                price: parseInt($scope.newcar.price),
+                available: true
+            });
+
+        $scope.newcar = {};
+        //$scope.newcar.name = "";
+        //$scope.newcar.color = "";
+        //$scope.newcar.price = "";
+
+        };
+        $scope.ninjas = [
+            {
+            name: 'Honda',
+            color: 'Red',
+            price: 19000,
+            available: true,
+            thumb: "content/img/homer.jpg"
+            },
+            {name: 'Toyota',
+            color: 'Black',
+            price: 18000,
+            available: true,
+            thumb: "content/img/marge.jpg"
+            },
+            {name: 'Subaru',
+            color: 'Blue',
+            price: 22000,
+            available: true,
+            thumb: "content/img/bart.jpg"
+            },
+            {name: 'Nissan',
+            color: 'Orange',
+            price: 25000,
+            available: true,
+            thumb: "content/img/lisa.jpg"
+            },
+        ];
+}]);
